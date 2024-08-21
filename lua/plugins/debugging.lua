ec0232d66b300302f1f12528f6d55b83d19afd43
DDvals <d.valsecchi88@gmail.com>
2024-08-21 11:08:38 +0200

    keymap

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,30 +1,36 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
     "nvim-neotest/nvim-nio",
-    "leoluz/nvim-dap-go"
-	},
-	config = function()
-		local dap = require("dap")
+    "leoluz/nvim-dap-go",
+  },
+  config = function()
+    local dap = require("dap")
     local dapui = require("dapui")
     dapui.setup()
     require("dap-go").setup()
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
 
-		vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<leader>dc", dap.continue, {})
-	end,
+    vim.keymap.set("n", "<F5>", dap.continue)
+    vim.keymap.set("n", "<F10>", dap.step_over)
+    vim.keymap.set("n", "<F11>", dap.step_into)
+    vim.keymap.set("n", "<F12>", dap.step_out)
+    vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+    vim.keymap.set("n", "<leader>B", function()
+      dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+    end)
+  end,
 }
