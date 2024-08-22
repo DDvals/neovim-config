3b5aa3a24cb59563383c9ed9c4116016cc210ec0
Daniele Valsecchi <dvalsecchi@softeam.it>
2024-08-22 17:33:06 +0200

    treesitter yaml

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local config = require("nvim-treesitter.configs")
         config.setup({
             auto_install = true,
-            ensure_installed = {"lua", "javascript", "json", "jsonc"},
+            ensure_installed = {"lua", "javascript", "json", "jsonc", "yaml"},
             highlight = { enable = true },
             indent = { enable = true },
         })
