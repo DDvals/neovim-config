7f18735b0630847ec9c4d53ef018185b55e35bb0
Daniele Valsecchi <dvalsecchi@softeam.it>
2024-08-23 10:40:13 +0200

    bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      vim.opt.termguicolors = true
+      local bufferline = require("bufferline")
+      bufferline.setup({
+        options = {
+          diagnostics = "nvim_lsp",
+          diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local icon = level:match("error") and " " or " "
+            return " " .. icon .. count
+          end,
+        },
+      })
+    end,
+  },
+}
