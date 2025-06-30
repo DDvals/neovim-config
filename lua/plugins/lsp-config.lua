e24f2c3b8b9650bab476efdf0f167fecc2d3213a
Daniele Valsecchi <danzival@gmail.com>
2025-06-30 11:43:18 +0200

    diagnostic

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -27,6 +27,18 @@ return {
       lspconfig.ts_ls.setup({
         capabilities = capabilities
       })
+	  
+	  vim.diagnostic.config({
+	    virtual_text = {
+		  spacing = 4,
+		  prefix = "●",
+	    },
+	    signs = true,
+	    underline = true,
+	    update_in_insert = false,
+	    severity_sort = true,
+	  })	
+	  
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
