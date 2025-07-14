94b13a35f09557ba44f699fc5e273ca67270e1dc
Daniele Valsecchi <danzival@gmail.com>
2025-07-14 16:59:27 +0200

    added kanawaga colorscheme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,9 +1,14 @@
 
 return { 
-    "catppuccin/nvim", 
-    name = "catppuccin", 
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme "catppuccin"
-    end
+    {"catppuccin/nvim", name = "catppuccin", lazy = true --[[, priority = 1000, 
+        config = function()
+            vim.cmd.colorscheme "catppuccin"
+        end
+        ]]
+    },
+    {"rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000,
+        config = function()
+            vim.cmd.colorscheme "kanagawa"
+        end
+    }
 }
