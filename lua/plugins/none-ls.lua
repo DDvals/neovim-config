55bd345555cb3c8c2155bf15cf0442dd1f910fa3
Daniele Valsecchi <danzival@gmail.com>
2025-07-21 16:02:30 +0200

    disabled js formatting and code actions

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,8 +8,8 @@ return {
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.code_actions.ts_node_action,
+                --null_ls.builtins.formatting.prettier,
+                --null_ls.builtins.code_actions.ts_node_action,
                 --null_ls.builtins.code_actions.refactoring,
                 --null_ls.builtins.completion.spell,
                 --null_ls.builtins.diagnostics.codespell,
