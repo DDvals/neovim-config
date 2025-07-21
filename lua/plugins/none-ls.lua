bf9140d8c70b144e67ce6ddf36304a5f472e8695
Daniele Valsecchi <danzival@gmail.com>
2025-07-21 15:18:21 +0200

    added disabled none_ls builtins

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,18 +1,28 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        require("none-ls.diagnostics.eslint_d"),
-		require("none-ls.code_actions.eslint"),
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {desc = "LSP format"})
-  end,
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.code_actions.ts_node_action,
+                --null_ls.builtins.code_actions.refactoring,
+                --null_ls.builtins.completion.spell,
+                --null_ls.builtins.diagnostics.codespell,
+                --null_ls.builtins.diagnostics.cppcheck,
+                --null_ls.builtins.diagnostics.opentofu_validate,
+                --null_ls.builtins.diagnostics.terraform_validate,
+                --null_ls.builtins.diagnostics.semgrep,
+                --null_ls.builtins.diagnostics.todo_comments,
+                --null_ls.builtins.diagnostics.trail_space,
+                require("none-ls.diagnostics.eslint_d"),
+                require("none-ls.code_actions.eslint_d"),
+            },
+        })
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "LSP format" })
+    end,
 }
