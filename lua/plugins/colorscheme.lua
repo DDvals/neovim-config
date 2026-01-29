20202dc509e29d52bb82077035764ea31652f8f9
Daniele Valsecchi <danzival@gmail.com>
2026-01-29 13:23:01 +0100

    everforest color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,27 @@
-
-return { 
-    {"catppuccin/nvim", name = "catppuccin", lazy = true --[[, priority = 1000, 
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = true --[[, priority = 1000,
         config = function()
             vim.cmd.colorscheme "catppuccin"
         end
-        ]]
-    },
-    {"rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000,
+        ]],
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		name = "kanagawa" --[[, priority = 1000,
         config = function()
             vim.cmd.colorscheme "kanagawa"
         end
-    }
+        ]],
+	},
+	{
+		"neanias/everforest-nvim",
+		name = "Everforest",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("everforest")
+		end,
+	},
 }
