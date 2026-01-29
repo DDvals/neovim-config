79bf22717fa6ea1c9ff57b2407710d896682239f
Daniele Valsecchi <danzival@gmail.com>
2026-01-29 13:22:31 +0100

    neotree cfg

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,11 +2,22 @@ return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
 	dependencies = {
-	  "nvim-lua/plenary.nvim",
-	  "nvim-tree/nvim-web-devicons",
-	  "MunifTanjim/nui.nvim",
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
 	},
-    config = function()
-        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left <CR>', {})
-    end
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left <CR>", {})
+	end,
+	opts = {
+		use_libuv_file_watcher = true,
+		follow_current_file = { enabled = true },
+		clipboard = { sync = "global" },
+		filesystem = {
+			filtered_items = {
+				hide_gitignored = false,
+				hide_dotfiles = false,
+			},
+		},
+	},
 }
