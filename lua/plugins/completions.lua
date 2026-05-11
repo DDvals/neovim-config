38ce5ec8d0189c31b801305ba448d9ad153edbd8
Daniele Valsecchi <danzival@gmail.com>
2026-05-11 12:13:38 +0200

    cmp config

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,62 +1,65 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-nvim-lsp-signature-help",
+        "hrsh7th/cmp-buffer", -- nvim-cmp source for words from the current buffer
+		"hrsh7th/cmp-path", -- nvim-cmp source for filesystem paths
+        "hrsh7th/cmp-nvim-lsp-signature-help", -- function signatures
+        {
+            "L3MON4D3/LuaSnip",
+            dependencies = {
+                "saadparwaiz1/cmp_luasnip",
+                "rafamadriz/friendly-snippets",
+            }
+        },
+    },
+    config = function()
+        local cmp = require("cmp")
+        require("luasnip.loaders.from_vscode").lazy_load()
 
-			local has_words_before = function()
-				if vim.bo[0].buftype == "prompt" then
-					return false
-				end
-				local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-			end
+        local has_words_before = function()
+            if vim.bo[0].buftype == "prompt" then
+                return false
+            end
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0
+                and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+        end
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					-- for copilot autosuggestions
-					["<Tab>"] = vim.schedule_wrap(function(fallback)
-						if cmp.visible() and has_words_before() then
-							cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-						else
-							fallback()
-						end
-					end),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-					{ name = "copilot" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                -- for copilot autosuggestions
+                ["<Tab>"] = vim.schedule_wrap(function(fallback)
+                    if cmp.visible() and has_words_before() then
+                        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                    else
+                        fallback()
+                    end
+                end),
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "copilot" },
+                { name = "buffer" },
+                { name = "path" },
+				{ name = "nvim_lsp_signature_help" },
+            }),
+        })
+    end,
 }
