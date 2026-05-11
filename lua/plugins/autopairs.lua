1097a6ccb80e6915fdf7323856c1fa8659282659
Daniele Valsecchi <danzival@gmail.com>
2026-05-11 14:28:12 +0200

    autopairs

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,16 @@
+return {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+        local autopairs = require("nvim-autopairs")
+        autopairs.setup({
+            check_ts = true,
+            map_cr = false, -- handled manually in nvim-cmp CR mapping
+        })
+
+        -- Integration with nvim-cmp
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        local cmp = require("cmp")
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+}
